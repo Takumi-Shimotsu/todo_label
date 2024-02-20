4e6a65d9f109f7cb63ab95eac66a7a11cae0b108
Takumi-Shimotsu <txiajin@gmail.com>
2024-02-20 14:10:59 +0900

    仮page作成

---
+++ src/app/login/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Login() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-12">
+      <p>サンプル2</p>
+    </main>
+  );
+}
